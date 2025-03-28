d2f7a997248f290bf6632655d628b7444b452543
Desmond <desmondtyz-pm24@student.tarc.edu.my>
2025-03-28 21:46:25 +0800

    bye

--- Doctordetails.java
+++ Doctordetails.java
@@ -4,4 +4,58 @@ public class Doctordetails {
     private String doctorGender;
     private int doctorContactNumber;
     private String doctorAddress;
+
+    //constructor 
+    public Doctordetails(String doctorID, String doctorName, String doctorGender, int doctorContactNumber, String doctorAddress){
+        this.doctorID = doctorID;
+        this.doctorName = doctorName;
+        this.doctorGender = doctorGender;
+        this.doctorContactNumber = doctorContactNumber;
+        this.doctorAddress = doctorAddress;
+    }
+
+    //set 
+    public void setDoctorID(String doctorID){
+        this.doctorID = doctorID;
+    }
+
+    public void setDoctorName(String doctorName){
+        this.doctorName = doctorName;
+    }
+
+    public void setDoctorGender(String doctorGender){
+        this.doctorGender = doctorGender;
+    }
+
+    public void setDoctorContactNumber(int doctorContactNumber){
+        this.doctorContactNumber = doctorContactNumber;
+    }
+
+    public void setDoctorAddress(String doctorAddress){
+        this.doctorAddress = doctorAddress;
+    }
+
+    //get method 
+    public String getDoctorName(){
+        return doctorName;
+    }
+
+    public String getDoctorID(){
+        return doctorID;
+    }
+
+    public String getDoctorGender(){
+        return doctorGender;
+    }
+
+    public int getDoctorContactNumber(){
+        return doctorContactNumber;
+    }
+
+    public String getDoctorAddress(){
+        return doctorAddress;
+    }
+    
+    
+    
 }
