72d26414fc8649b3151bdd793feb56ecdc0144b5
Desmond <desmondtyz-pm24@student.tarc.edu.my>
2025-04-19 14:04:25 +0800

    push

--- Doctordetails.java
+++ Doctordetails.java
@@ -72,6 +72,7 @@ public class Doctordetails extends Staff{
 }
 
 =======
+<<<<<<< HEAD
 public class Doctordetails {
     private String doctorID;
     private String doctorName;
@@ -133,4 +134,67 @@ public class Doctordetails {
     
     
 }
+=======
+public class Doctordetails {
+    private String doctorID;
+    private String doctorName;
+    private String doctorGender;
+    private int doctorContactNumber;
+    private String doctorAddress;
+
+    //constructor 
+    public Doctordetails(String doctorID, String doctorName, String doctorGender, int doctorContactNumber, String doctorAddress){
+        this.doctorID = doctorID;
+        this.doctorName = doctorName;
+        this.doctorGender = doctorGender;
+        this.doctorContactNumber = doctorContactNumber;
+        this.doctorAddress = doctorAddress;
+    }
+
+    //set 
+    public void setDoctorID(String doctorID){
+        this.doctorID = doctorID;
+    }
+
+    public void setDoctorName(String doctorName){
+        this.doctorName = doctorName;
+    }
+
+    public void setDoctorGender(String doctorGender){
+        this.doctorGender = doctorGender;
+    }
+
+    public void setDoctorContactNumber(int doctorContactNumber){
+        this.doctorContactNumber = doctorContactNumber;
+    }
+
+    public void setDoctorAddress(String doctorAddress){
+        this.doctorAddress = doctorAddress;
+    }
+
+    //get method 
+    public String getDoctorName(){
+        return doctorName;
+    }
+
+    public String getDoctorID(){
+        return doctorID;
+    }
+
+    public String getDoctorGender(){
+        return doctorGender;
+    }
+
+    public int getDoctorContactNumber(){
+        return doctorContactNumber;
+    }
+
+    public String getDoctorAddress(){
+        return doctorAddress;
+    }
+    
+    
+    
+}
+>>>>>>> 1f7ec8c51d2b7c48cf8d8cfdd791406fda18806b
 >>>>>>> d2f7a997248f290bf6632655d628b7444b452543
--- Patientdetails.java
+++ Patientdetails.java
@@ -150,6 +150,7 @@ public class Patientdetails{
     }
 
 =======
+<<<<<<< HEAD
 public class Patientdetails{
     private String patientID;
     private String patientName;
@@ -223,5 +224,80 @@ public class Patientdetails{
 
 
 
+=======
+public class Patientdetails{
+    private String patientID;
+    private String patientName;
+    private String patientAddress;
+    private int patientPhoneNumber;
+    private int emergencyContact;
+    private String medicalHistory;
+
+    //set patientID
+    public void setPatientID(String patientID){
+        this.patientID = patientID;
+    }
+
+    //set patientname 
+    public void setPatientName(String patientName){
+        this.patientName = patientName;
+    }
+
+    // set patient address
+    public void setPatientAddress(String patientAddress){
+        this.patientAddress = patientAddress;
+    }
+
+    //set patient phone number
+    public void setpatientPhoneNumber(int patientPhoneNumber){
+        this.patientPhoneNumber = patientPhoneNumber;
+    }
+
+    //set emergency contact number 
+    public void setPatientEmergencyNumber(int emergencyContact){
+        this.emergencyContact = emergencyContact;
+    }
+
+    //set medical history
+    public void setMedicalHistory(String medicalHistory){
+        this.medicalHistory = medicalHistory;
+    }
+
+    //get patientID
+    public String getPatientID(){
+        return patientID;
+    }
+
+    //get patient name 
+    public String getPatientName(){
+        return patientName;
+    }
+
+    //get patient address
+    public String getPatientAddress(){
+        return patientAddress;
+    }
+
+    //get patient phone number 
+    public int getPatientPhoneNumber(){
+        return patientPhoneNumber;
+    }
+
+    //get emergency contact 
+    public int getEmergencyContact(){
+        return emergencyContact;
+    }
+
+    //get medical history
+    public String getMedicalHistory(){
+        return medicalHistory;
+    }
+
+
+
+
+
+
+>>>>>>> 1f7ec8c51d2b7c48cf8d8cfdd791406fda18806b
 >>>>>>> d2f7a997248f290bf6632655d628b7444b452543
 }
